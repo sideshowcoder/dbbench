be8b7453194359dea45c0c46a036b2c4bb419175
Philipp Fehre <philipp.fehre@googlemail.com>
2012-06-11 12:22:47 +0200

    Finished Data Generator

--- lib/db_bench/adapters/mysql_adapter/generator.rb
+++ lib/db_bench/adapters/mysql_adapter/generator.rb
@@ -1,4 +1,5 @@
 require_relative "generated_lift"
+require "ffaker"
 
 module DbBench
   module Mysql
@@ -7,16 +8,15 @@ module DbBench
       
       SQL_TYPE_TO_DATA_MAPPING = {
         :md5 => /^varchar\(40\)/,
-        :id => /^int\(10\)/,
         :tinyint => /^tinyint(\((\d+)\))?\s?(unsigned)?/,
         :enum => /^enum\(('([^,]+)',?)+\)/,
-        :date => "date",
-        :time => "time",
+        :date => /^date/,
+        :time => /^time/,
         :smallint => /^smallint(\((\d+)\))?\s?(unsigned)?/,
         :int => /^int(\((\d+)\))?\s?(unsigned)?/ ,
-        :decimal => "decimal",
-        :datetime => "datetime",
-        :double => "double",
+        :decimal => /^decimal\((\d+),(\d+)\)/,
+        :datetime => /^datetime/,
+        :double => /^double/,
         :varchar => /^varchar\((\d+)\)/
       }
       
@@ -26,8 +26,7 @@ module DbBench
           hash[key] = value.sql_type
           hash
         end
-        p build_data column_data
-        # GeneratedLift.create! build_data column_data
+        GeneratedLift.create! build_data column_data
       end
       
       private 
@@ -37,10 +36,16 @@ module DbBench
               if (m = matcher.match type)
                 if [:varchar, :md5].include? func
                   hash[field] = send func, *m[1]
+                elsif [:decimal].include? func
+                  hash[field] = send func, *m[1..2]
                 elsif [:int, :tinyint, :smallint].include? func
                   hash[field] = send func, *m[2..m.length]
                 elsif [:enum].include? func
                   hash[field] = send func, type.split(",").map { |e| /'(.+)'/.match(e)[1] }
+                elsif [:double, :date, :time, :datetime].include? func
+                  hash[field] = send func
+                else
+                  p m
                 end
                 break
               end
@@ -55,30 +60,48 @@ module DbBench
         end
         
         def varchar chars
+          Faker::Lorem.words(30).join[0..chars.to_i]
         end
         
         def tinyint limit, unsigned=false
+          int limit, unsigned, 127 
         end
         
         def smallint limit, unsigned=false
+          int limit, unsigned, 32768 
         end
         
-        def int limit, unsigned=false
+        def int limit, unsigned=false, max_signed=2147483647
+          limit = limit.to_i() -1
+          max_unsigned = max_signed*2
+          if unsigned
+            val = rand max_unsigned
+          else
+            val = rand(2) == 0 ? rand(max_signed) : -(rand max_signed)        
+          end
+          val.to_s[0..limit].to_i
         end
         
         def decimal digits, scale
+          scale = scale.to_i
+          digits = digits.to_i() - scale
+          rand.to_s[0..scale].to_f() + int(digits)
         end
         
         def double
+          rand(100000000000000010901051724930857196452234783424494612613028642816) + rand
         end
         
         def date
+          Date.new
         end
         
         def time
+          Time.now
         end
         
         def datetime
+          DateTime.now
         end
         
         def enum options
