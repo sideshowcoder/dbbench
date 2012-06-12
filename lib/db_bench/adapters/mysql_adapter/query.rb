a0b6e0dd0d201e858f98c46bf4d2c80331545239
Philipp Fehre <philipp.fehre@googlemail.com>
2012-06-12 18:32:12 +0200

    Updated Working Query

--- lib/db_bench/adapters/mysql_adapter/query.rb
+++ lib/db_bench/adapters/mysql_adapter/query.rb
@@ -15,9 +15,89 @@ module DbBench
         def build_query_string args
           # remove NULL Values
           query = args.delete_if { |key, val| val.to_s.match /NULL/ }
-          query.map do |key, val|
-            val.class == String ? "#{key} = '#{val}'" : "#{key} = #{val}"
-          end.join " AND " 
+          query_conditions = []
+          
+          # Radius Search
+          if destination_id = query["destination_id"].to_i
+            # calculate the radius to search for destination_radius
+            if (destination_radius = query["destination_radius"].to_i) && destination_radius != 0
+              # get lat and long
+              lift = GeneratedLift.find_by_destination_id destination_id
+              destination_lat = lift.destination_lat
+              destination_long = lift.destination_long
+              
+              query.delete "destination_long"
+              query.delete "destination_lat"
+              query.delete "destination_radius"
+              
+              if destination_long.nil? || destination_lat.nil?
+                # No Long Lat for those so cant do radius search
+              else
+                # Clear from query
+                query.delete "destination_id"                
+                # Add Condition
+                query_conditions << calculate_radius(destination_lat, destination_long, destination_radius, "destination")
+              end
+            end
+          end          
+                    
+          if departure_id = query["departure_id"].to_i
+            # calculate the radius to search for destination_radius
+            if (departure_radius = query["departure_radius"].to_i) && departure_radius != 0
+              # get lat and long
+              lift = GeneratedLift.find_by_departure_id departure_id
+              departure_lat = lift.departure_lat
+              departure_long = lift.departure_long
+              
+              query.delete "departure_long"
+              query.delete "departure_lat"
+              query.delete "departure_radius"
+              
+              if departure_lat.nil? || departure_long.nil?
+                 # No Long Lat for those so cant do radius search
+               else      
+                 # Clear from query
+                 query.delete "departure_id"
+                 # Add Condition
+                 query_conditions << calculate_radius(departure_lat, departure_long, departure_radius, "departure")           
+               end
+            end
+          end
+          
+          # Date Search
+          if base_date = query["search_date"]
+            query["date"] = base_date
+            query.delete "search_date"
+          end
+          
+          if search_date_range = query["search_date_range"]
+            query.delete "search_date_range"
+          end
+          
+          if search_date_range != 0 && base_date
+            base_date = Date.parse base_date
+            query_conditions << "(date BETWEEN ''#{base_date.to_s}' AND '#{(base_date + search_date_range.to_i).to_s}')" 
+          end
+          
+          # Remaining query parameters
+          if !query.empty?
+            query_conditions << query.map { |key, val| val.class == String ? "#{key} = '#{val}'" : "#{key} = #{val}" }
+          end
+          
+          query_conditions.join " AND "
+        end
+        
+        def calculate_radius lat, long, radius, label
+          lat1km = 1/111.0
+          long1km = (1 / (Math::cos( deg2rad lat ) * 111)).abs
+
+          "(#{label}_lat BETWEEN #{lat - (lat1km * radius)} AND #{lat + (lat1km * radius)}) AND
+          (#{label}_long BETWEEN #{long - (long1km * radius)} AND #{long + (long1km * radius)}) AND
+          (POW((abs(#{label}_lat - #{lat})/#{lat1km}),2))+(POW((abs(#{label}_long-#{long})/#{long1km}),2)) <= #{radius**2}"
+        end
+        
+        def deg2rad th
+          th / 180.0 * Math::PI
         end
   
     end
